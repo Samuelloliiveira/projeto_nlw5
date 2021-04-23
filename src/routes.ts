effc3f08f917b0fefbcd495867168e9dc7ea692a
samuel <samuelcontatodev@gmail.com>
2021-04-23 12:53:59 -0300

    trabalhando com cadastro, envio, rotas, construtores, etidades, tabelas

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import {Router} from "express"
+import { MessagesController } from "./controllers/MessagesController"
 import { SettingsController } from "./controllers/SettingsController"
 import { UsersController } from "./controllers/UsersController"
 
@@ -6,8 +7,13 @@ const routes = Router()
 
 const settingsController = new SettingsController()
 const usersCOntroller = new UsersController()
+const messagesController = new MessagesController()
 
 routes.post("/settings", settingsController.create)
 routes.post("/users", usersCOntroller.create)
+
+routes.post("/messages", messagesController.create)
+routes.get("/messages/:id", messagesController.showByUser)
+
 
 export{routes}
