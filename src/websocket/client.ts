7a9f7d971a21bf7b456176085fbee5a6c18ac2ea
samuel <samuelcontatodev@gmail.com>
2021-04-25 20:38:33 -0300

    Trabalhando html atraves do Mustache (nao foi muito aprofundado), criando toda a configuraçao do admin

--- src/websocket/client.ts
+++ src/websocket/client.ts
@@ -51,5 +51,9 @@ io.on("connect", (socket) => {
             text,
             user_id,
         })
+
+        const allMessages = await messagesService.listByUser(user_id)
+
+        socket.emit("client_list_all_messages", allMessages)
     })
 })
